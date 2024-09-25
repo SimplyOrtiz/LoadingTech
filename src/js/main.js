d32a0b7b732b741140bdf1720bc8aa7d20784c37
Ortiz <guilherme.ortiz.franca@escola.pr.gov.br>
2024-09-25 15:14:40 +0000

    fixed issues in js code

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,45 @@
 var changeButton = document.getElementById("Change")
 
 if (localStorage.getItem("darkMode") === null) {
-    localStorage.setItem('darkMode', 'true')
+    localStorage.setItem('darkMode', true)
 }
 
 var darkMode = localStorage.getItem('darkMode')
+var localdarkMode;
 
 
 function clickHandler(){
-    if (darkMode == true){
+    if (localdarkMode == true){
         document.body.classList.remove('dark-mode')
         document.body.classList.add('light-mode')
 
-        localStorage.setItem('darkMode', 'false')
+        localdarkMode = false
+        localStorage.setItem('darkMode', false)
+        console.log(localdarkMode,"\n",darkMode);
     } else {
         document.body.classList.remove('light-mode')
         document.body.classList.add('dark-mode')
 
-        localStorage.setItem('darkMode', 'true')
+        localdarkMode = true
+        localStorage.setItem('darkMode', true)
+        console.log(localdarkMode,"\n",darkMode);
     }
 }
 
-localStorage.setItem('used', 'true')
-
 changeButton.addEventListener('click', clickHandler);
 
-
 window.onload = function(){
-    clickHandler()
+    if (darkMode == true){
+        document.body.classList.remove('dark-mode')
+        document.body.classList.add('light-mode')
+    } else {
+        document.body.classList.remove('light-mode')
+        document.body.classList.add('dark-mode')
+    }
+
+    if (darkMode == true){
+        localdarkMode = true
+    } else {
+        localdarkMode = false
+    }
 }
