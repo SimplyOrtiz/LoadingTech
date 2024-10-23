0a07b73c57b1f9d020d11c313839e938ff8aad69
Ortiz <guilherme.ortiz.franca@escola.pr.gov.br>
2024-10-23 12:04:49 -0300

    Uploaded code changes
    Uploaded the lastest code changes to the repository

---
+++ LoadingTech-indev-bootstrap/src/js/buttons.js
@@ -0,0 +1,9 @@
+document.getElementById("read-more").onclick = function () {
+    location.href = "./subpages/articles.html"
+}
+
+document.getElementById("watch-video-dow").onclick = function () {
+    window.open(
+        "https://www.youtube.com/watch?v=SzDhV_PLHvw"
+    )
+}
